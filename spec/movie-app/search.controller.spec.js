06a98ed1f522483ebee94c4516e9bb62ae7bf6bf
vagrant <vagrant@homestead>
2017-08-02 20:08:41 +0200

    added functionality to search movie while typing name

--- spec/movie-app/search.controller.spec.js
+++ spec/movie-app/search.controller.spec.js
@@ -3,15 +3,18 @@ describe('search controller', function() {
 	var $scope;
 	var $location;
 	var $controller;
+	var $timeout;
 
 	beforeEach(module('movieApp'));
-	beforeEach(inject(function(_$controller_, _$location_) {
+	/*beforeEach(inject(function(_$controller_, _$location_, _$timeout_) {
 		$controller = _$controller_;
 		$location = _$location_;
+		$timeout = _$timeout_;
 		$scope = {};
-	}));
-	/*beforeEach(inject(function(_$controller_, _$location_) {
+	}));*/
+	beforeEach(inject(function(_$controller_, _$location_, _$timeout_) {
 		$location = _$location_;
+		$timeout = _$timeout_;
 		$scope = {};
 		//var fn = function() {
 		//	$scope.search = function() {
@@ -22,14 +25,14 @@ describe('search controller', function() {
 		//	}
 		//};
 		//_$controller_(fn, { $scope: $scope, $location: $location});
-		_$controller_('SearchController', { $scope: $scope, $location: $location});
-	}));*/
+		_$controller_('SearchController', { $scope: $scope, $location: $location, $timeout: $timeout});
+	}));
 	
 	it('should redirect to the query results page for non-empty query', function() {
 		var query = 'star wars';
 		// controller(<controller name>, locals[, bindings])
 		//$this = $controller('SearchController', {$location: $location }, {query: 'star wars'});
-		$controller('SearchController', { $scope: $scope, $location: $location }, {query: query});
+		//$controller('SearchController', { $scope: $scope, $location: $location }, {query: query});
 		$scope.query = query;
 		$scope.search();
 		expect($location.url()).toBe('/results?q=' + encodeURIComponent(query));
@@ -37,10 +40,35 @@ describe('search controller', function() {
 	
 	it('should not redirect to the query results page for empty query', function() {
 		//$this = $controller('SearchController', {$location: $location }, {query: ''});
-		$controller('SearchController', {$scope: $scope, $location: $location }, {query: ''});
+		//$controller('SearchController', {$scope: $scope, $location: $location }, {query: ''});
 		$scope.query = '';
 		$scope.search();
 		expect($location.url()==='/'?'':'').toBe('');
+	})
+	
+	it('should redirect after 1 second keyup inactivity', function(){
+		//$controller('SearchController', {$scope: $scope, $location: $location, $timeout: $timeout }, {query: 'star wars'});
+		$scope.query = 'star wars';
+		$scope.keyup();
+		$timeout.flush();
+		expect($timeout.verifyNoPendingTasks).not.toThrow();
+		expect($location.url()).toBe('/results?q=star%20wars');
+	})
+
+	it('should cancel timeout after keydown', function(){
+		//$controller('SearchController', {$scope: $scope, $location: $location, $timeout: $timeout }, {query: 'star wars'});
+		$scope.query = 'star wars';
+		$scope.keyup();
+		$scope.keydown();
+		expect($timeout.verifyNoPendingTasks).not.toThrow();
+	})
+	
+	it('should cancel timeout on search', function(){
+		//$controller('SearchController', {$scope: $scope, $location: $location, $timeout: $timeout }, {query: 'star wars'});
+		$scope.query = 'star wars';
+		$scope.keyup();
+		$scope.search();
+		expect($timeout.verifyNoPendingTasks).not.toThrow();
 	})
 
 });
--- src/movie-app/search.controller.js
+++ src/movie-app/search.controller.js
@@ -1,9 +1,18 @@
 angular.module('movieApp')
-	.controller('SearchController', function($scope, $location) {
+	.controller('SearchController', function($scope, $location, $timeout) {
+		var timeout;
+		$scope.keyup = function() {
+			timeout = $timeout($scope.search, 1000);
+		};
+		$scope.keydown = function() {
+			$timeout.cancel(timeout);
+			timeout = false;
+		};
 		$scope.search = function() {
 		//$scope.search = function() {
 			console.log('location.url='+$location.url());
 			console.log('scope.query=' + $scope.query);
+			if(timeout) $timeout.cancel(timeout);
 			if($scope.query) {
 				$location.path('/results').search('q', $scope.query);
 			}
